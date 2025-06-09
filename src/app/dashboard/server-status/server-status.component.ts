71ff6eef92b7ec6a56ffa136a3c63adadaa96098
JacksonSmith43 <jessica444@gmx.at>
2025-06-09 11:30:13 +0200

    121. Manipulating State & Using Literal Values.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online'; // Only online, offline or unknown are accepted. 
+
+  constructor() {
+    setInterval(() => {
+      const random = Math.random(); // Generates a number between 0 and 0.9999999.
+
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+
+      } else {
+        this.currentStatus = 'unknown';
+      }
+
+    }, 500)
+  }
 }
