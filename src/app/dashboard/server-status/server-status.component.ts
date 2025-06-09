8eea0ac27675fa2ae5e625f4beeff4f50b8e0977
JacksonSmith43 <jessica444@gmx.at>
2025-06-09 13:35:42 +0200

    125. Component Cleanup with ngOnDestroy.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,15 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, AfterViewInit { // implements OnInit is here, so that if ngOnInit were to be spellt incorrectly, the Compiler would pick up on it. 
+
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy { // implements OnInit is here, so that if ngOnInit were to be spellt incorrectly, the Compiler would pick up on it. 
   currentStatus: 'online' | 'offline' | 'unknown' = 'online'; // Only online, offline or unknown are accepted. 
+  private interval?: ReturnType<typeof setInterval>; // This will return exactly the same type as setInterval, which is a number in the browser, but a NodeJS.Timeout in NodeJS.
 
   ngOnInit() { // Will be loaded once Angular has initialised all of the components inputs. 
     console.log("ngOnInit");
-    setInterval(() => {
+
+    this.interval = setInterval(() => {
       const random = Math.random(); // Generates a number between 0 and 0.9999999.
 
       if (random < 0.5) {
@@ -30,5 +33,9 @@ export class ServerStatusComponent implements OnInit, AfterViewInit { // impleme
 
   ngAfterViewInit() {
     console.log("ngAfterViewInit");
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
